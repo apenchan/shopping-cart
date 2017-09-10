4f87f050a7d08013c481e8ee083a1d956b7bc596
anna Penchansky <annapenchansky@annas-MacBook-Pro.local>
2017-09-10 10:40:32 +0300

    added express, npm installed, have the backend running, put my js and css in a public folder

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+	res.sendfile('./index.html');
+});
+
+app.listen(port);
+console.log("==========================");
+console.log("Server is running on Shopping Cart" + port);
+console.log("===========================");
